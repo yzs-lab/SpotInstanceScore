142093d1b541031d72bd18bab113112fbb73627a
Tizen Ye <yzs981130@126.com>
2021-11-15 20:25:52 +0800

    feat() more configurations and upload result

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module sps
 go 1.16
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.11.0 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.11.0
 	github.com/aws/aws-sdk-go-v2/config v1.10.1
+	github.com/aws/aws-sdk-go-v2/feature/s3/manager v1.7.1
 	github.com/aws/aws-sdk-go-v2/service/ec2 v1.22.0
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.19.0
 )
--- main.go
+++ main.go
@@ -5,12 +5,20 @@ import (
 	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"log"
+	"os"
+	"strings"
+	"time"
 )
 
 var client *ec2.Client
+var s3client *s3.Client
 var azID map[string]string
+var now string
+var outputFileStr string
 
 func init() {
 	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-west-2"))
@@ -18,6 +26,7 @@ func init() {
 		log.Fatal(err)
 	}
 	client = ec2.NewFromConfig(cfg)
+	s3client = s3.NewFromConfig(cfg)
 
 
 	azID = make(map[string]string)
@@ -36,7 +45,9 @@ func init() {
 			azID[aws.ToString(az.ZoneId)] = aws.ToString(az.ZoneName)
 		}
 	}
-	fmt.Println(len(azID))
+
+	now = fmt.Sprint(time.Now().Format(time.RFC3339))
+	outputFileStr = "time,region,instance,azID,azName,capacity,single,score\n"
 }
 
 type spotPlacementScoresCase struct {
@@ -62,31 +73,91 @@ func querySpotPlacementScores(targetCapacity int32, instanceTypes []string, regi
 	}
 
 	for _, result := range output.SpotPlacementScores {
-		fmt.Printf("%s %s %s %d\n", aws.ToString(result.Region), aws.ToString(result.AvailabilityZoneId), azID[aws.ToString(result.AvailabilityZoneId)], aws.ToInt32(result.Score))
+		// outputFileStr = "time,region,instance,azID,azName,capacity,single,score\n"
+		s := fmt.Sprintf("%s,%s,%s,%s,%s,%d,%v,%d\n",
+			now,
+			aws.ToString(result.Region), strings.Join(instanceTypes, "+"),
+			aws.ToString(result.AvailabilityZoneId), azID[aws.ToString(result.AvailabilityZoneId)],
+			targetCapacity, singleAvailabilityZone, aws.ToInt32(result.Score))
+		outputFileStr += s
 	}
 }
 
+// var GPUInstanceTypes = []string{"p2.xlarge", "p2.8xlarge", "p2.16xlarge", "p3.2xlarge", "p3.8xlarge", "p3.16xlarge", "p3dn.24xlarge", "p4d.24xlarge"}
+
+
+var GPUInstanceTypes = []string{"p2.xlarge", "p2.8xlarge", "p3.2xlarge", "p3.8xlarge"}
+var regionNames = []string{"us-east-1", "us-east-2", "us-west-1", "us-west-2"}
+var singleAvailabilityZoneRange = []bool{true, false}
+var targetCapacityRange = []int32{1, 2, 4}
+
+func saveResult() {
+	filename := now + ".csv"
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	_, err = file.WriteString(outputFileStr)
+	if err != nil {
+		panic(err)
+	}
+
+
+	uploader := manager.NewUploader(s3client)
+	_, err = uploader.Upload(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(os.Getenv("bucket")),
+		Key:    aws.String(filename),
+		Body:   strings.NewReader(outputFileStr),
+	})
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	spotPlacementScoresCases = []spotPlacementScoresCase{
 		{
-			instanceTypes: []string{"p3.2xlarge", "p3.8xlarge", "p3.16xlarge"},
-			regionNames: []string{"us-east-1", "us-east-2", "us-west-1", "us-west-2"},
+			instanceTypes: GPUInstanceTypes,
+			regionNames: regionNames,
 			targetCapacity: int32(1),
 			singleAvailabilityZone: true,
-			name: "all GPU instance",
+			name: "all_1_true",
 		},
 		{
-			instanceTypes: []string{"p3.2xlarge"},
-			regionNames: []string{"us-east-1", "us-east-2", "us-west-1", "us-west-2"},
-			targetCapacity: int32(1),
+			instanceTypes: GPUInstanceTypes,
+			regionNames: regionNames,
+			targetCapacity: int32(2),
 			singleAvailabilityZone: true,
-			name: "p3.2xlarge",
+			name: "all_2_true",
 		},
+		{
+			instanceTypes: GPUInstanceTypes,
+			regionNames: regionNames,
+			targetCapacity: int32(4),
+			singleAvailabilityZone: true,
+			name: "all_4_true",
+		},
+	}
+
+	for _, instance := range GPUInstanceTypes {
+		for _, single := range singleAvailabilityZoneRange {
+			for _, capacity := range targetCapacityRange {
+				spotPlacementScoresCases = append(spotPlacementScoresCases, spotPlacementScoresCase{
+					instanceTypes:          []string{instance},
+					regionNames:            regionNames,
+					targetCapacity:         capacity,
+					singleAvailabilityZone: single,
+					name:                   fmt.Sprintf("%s_%d_%v", instance, capacity, single),
+				})
+			}
+		}
 	}
 
 	for _, spsCase := range spotPlacementScoresCases {
 		fmt.Println(spsCase.name)
 		querySpotPlacementScores(spsCase.targetCapacity, spsCase.instanceTypes, spsCase.regionNames, spsCase.singleAvailabilityZone)
 	}
+
+	saveResult()
 }
